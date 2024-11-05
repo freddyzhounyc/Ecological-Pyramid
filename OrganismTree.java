2d6e01bb2cee9ea816a44199b1bcf837cb40290e
Freddy Zhou <145275263+freddyzhounyc@users.noreply.github.com>
2024-11-05 18:23:01 -0500

    OrganismTree.java

--- OrganismTree.java
+++ OrganismTree.java
@@ -2,11 +2,6 @@
  * The OrganismTree class represents the tree that will contain the OrganismNodes and have the methods that perform different functions on the tree.
  *
  * @author Freddy Zhou
- *      Email: freddy.zhou@stonybrook.edu
- *      Stony Brook ID: 116580337
- *      HW #5 - Ecological Pyramid
- *      CSE 214
- *      Recitation Number: R04 | TA Names: Veronica Oreshko, Anuj Sureshbhai, Alex Zuzow
  */
 
 public class OrganismTree{
